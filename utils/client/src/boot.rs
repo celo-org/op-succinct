2beb6acd48d892759ca573046e2391b47fc58f25
seolaoh <osa8361@gmail.com>
2025-06-25 22:13:20 +0900

    Construct rollup config manually for hashing also

--- utils/client/src/boot.rs
+++ utils/client/src/boot.rs
@@ -15,7 +15,73 @@ pub const AGGREGATION_OUTPUTS_SIZE: usize = 6 * 32;
 /// on-chain, so switching to a different hash function is not a concern, as long as the config hash
 /// is consistent with the one on the contract.
 pub fn hash_rollup_config(config: &CeloRollupConfig) -> B256 {
-    let serialized_config = serde_json::to_string_pretty(config).unwrap();
+    // TODO: find cleaner way
+    let serialized_config = {
+      // Manually construct the JSON to match the RPC response format
+      let full_config = serde_json::json!({
+          "genesis": {
+              "l1": {
+                  "hash": format!("0x{:x}", config.op_rollup_config.genesis.l1.hash),
+                  "number": config.op_rollup_config.genesis.l1.number,
+              },
+              "l2": {
+                  "hash": format!("0x{:x}", config.op_rollup_config.genesis.l2.hash),
+                  "number": config.op_rollup_config.genesis.l2.number,
+              },
+              "l2_time": config.op_rollup_config.genesis.l2_time,
+              "system_config": config.op_rollup_config.genesis.system_config.as_ref().map(|sc| {
+                  serde_json::json!({
+                      "batcherAddr": format!("0x{:x}", sc.batcher_address),
+                      "overhead": format!("0x{:064x}", sc.overhead),
+                      "scalar": format!("0x{:064x}", sc.scalar),
+                      "gasLimit": sc.gas_limit,
+                      "eip1559Params": format!("0x{:016x}",
+                          (sc.eip1559_denominator.unwrap_or(0) as u64) |
+                          ((sc.eip1559_elasticity.unwrap_or(0) as u64) << 8)
+                      ),
+                      "operatorFeeParams": format!("0x{:064x}",
+                          (sc.operator_fee_scalar.unwrap_or(0) as u128) |
+                          ((sc.operator_fee_constant.unwrap_or(0) as u128) << 64)
+                      ),
+                  })
+              }),
+          },
+          "block_time": config.op_rollup_config.block_time,
+          "max_sequencer_drift": config.op_rollup_config.max_sequencer_drift,
+          "seq_window_size": config.op_rollup_config.seq_window_size,
+          "channel_timeout": config.op_rollup_config.channel_timeout,
+          "l1_chain_id": config.op_rollup_config.l1_chain_id,
+          "l2_chain_id": config.op_rollup_config.l2_chain_id,
+          "regolith_time": config.op_rollup_config.hardforks.regolith_time.unwrap_or(0),
+          // "cel2_time": config.hardforks.cel2_time.unwrap_or(0),
+          "canyon_time": config.op_rollup_config.hardforks.canyon_time.unwrap_or(0),
+          "delta_time": config.op_rollup_config.hardforks.delta_time.unwrap_or(0),
+          "ecotone_time": config.op_rollup_config.hardforks.ecotone_time.unwrap_or(0),
+          "fjord_time": config.op_rollup_config.hardforks.fjord_time.unwrap_or(0),
+          "granite_time": config.op_rollup_config.hardforks.granite_time.unwrap_or(0),
+          "holocene_time": config.op_rollup_config.hardforks.holocene_time.unwrap_or(0),
+          "isthmus_time": config.op_rollup_config.hardforks.isthmus_time.unwrap_or(0),
+          "batch_inbox_address": format!("0x{:x}", config.op_rollup_config.batch_inbox_address),
+          "deposit_contract_address": format!("0x{:x}", config.op_rollup_config.deposit_contract_address),
+          "l1_system_config_address": format!("0x{:x}", config.op_rollup_config.l1_system_config_address),
+          "protocol_versions_address": format!("0x{:x}", config.op_rollup_config.protocol_versions_address),
+          "chain_op_config": {
+              "eip1559Elasticity": config.op_rollup_config.chain_op_config.eip1559_elasticity,
+              "eip1559Denominator": config.op_rollup_config.chain_op_config.eip1559_denominator,
+              "eip1559DenominatorCanyon": config.op_rollup_config.chain_op_config.eip1559_denominator_canyon,
+          },
+          "alt_da": config.op_rollup_config.alt_da_config.as_ref().map(|alt_da| {
+              serde_json::json!({
+                  "da_challenge_contract_address": alt_da.da_challenge_address.map(|addr| format!("0x{addr:x}")),
+                  "da_commitment_type": alt_da.da_commitment_type.as_deref(),
+                  "da_challenge_window": alt_da.da_challenge_window,
+                  "da_resolve_window": alt_da.da_resolve_window,
+              })
+          }),
+      });
+      serde_json::to_string_pretty(&full_config).unwrap()
+    };
+    // let serialized_config = serde_json::to_string_pretty(config).unwrap();
 
     // Create a SHA256 hasher
     let mut hasher = Sha256::new();
