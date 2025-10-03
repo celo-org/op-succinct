a7ae1ea391009b486b15ec736bd773e3ea2942fd
Taehoon Kim <taehoon@succinct.xyz>
2025-10-03 15:15:56 -0700

    fix(fault-proof): fix proposer with canonical chain tracking (#613)
    * fix(fault-proof): fix proposer with canonical chain tracking
    
    * refactor: improve game resolution
    
    * refactor: improve game credit claim
    
    * refactor: improve game defense
    
    * chore: add comments
    
    * chore: rename to update_canonical_head_if_further
    
    * chore: rename to is_XXX for boolean returning func
    
    * refactor: remove early return from `refresh_state`
    
    * fix
    
    * fix
    
    * refactor: refactor fix-proposer PR (#627)
    
    * move run() to the top
    
    * move state init from refresh state to proposer init
    
    * huge refac
    
    * remove is_finalized
    
    * remove update_canonical_head_if_further
    
    * move below new
    
    * switch orders
    
    * compute canonical head
    
    * remove state snapshot
    
    * typo
    
    * refac remove subtree
    
    * fmt
    
    * update fetch_game
    
    * rename to sync_state
    
    * remove is_descendant_of_anchor
    
    * refac remove_subtree
    
    * more refac (#628)
    
    * wip
    
    * remove for claim_bond_for_game
    
    * fix
    
    * clippy
    
    * fix
    
    * fix
    
    * fix
    
    * clippy
    
    * fix
    
    * refac
    
    * fix: drop if parent game is not in cache
    
    * switch orders
    
    * fix is_parent_resolved
    
    * update testing doc
    
    * remove cursor and set_cursor
    
    * improve fetch_game
    
    * improve sync_games
    
    * misspell
    
    * test: add comments to e2e tests
    
    (cherry picked from commit 32f21401e7eca3c9222a3acd2f87be898a34e58d)

--- fault-proof/bin/proposer.rs
+++ fault-proof/bin/proposer.rs
@@ -6,7 +6,9 @@ use alloy_transport_http::reqwest::Url;
 use anyhow::Result;
 use clap::Parser;
 use fault_proof::{
-    config::ProposerConfig, contract::DisputeGameFactory, prometheus::ProposerGauge,
+    config::ProposerConfig,
+    contract::{AnchorStateRegistry, DisputeGameFactory, OPSuccinctFaultDisputeGame},
+    prometheus::ProposerGauge,
     proposer::OPSuccinctProposer,
 };
 use op_succinct_host_utils::{
@@ -36,6 +38,7 @@ async fn main() -> Result<()> {
 
     setup_logger();
 
+    let proposer_config = ProposerConfig::from_env()?;
     let proposer_signer = Signer::from_env()?;
 
     let l1_provider =
@@ -49,12 +52,11 @@ async fn main() -> Result<()> {
         l1_provider.clone(),
     );
 
-    // Use PROVER_ADDRESS from env if available, otherwise use wallet's default signer address from
-    // the private key.
-    let prover_address = env::var("PROVER_ADDRESS")
-        .ok()
-        .and_then(|addr| addr.parse::<Address>().ok())
-        .unwrap_or_else(|| proposer_signer.address());
+    let game_impl_address = factory.gameImpls(proposer_config.game_type).call().await?;
+    let game_impl = OPSuccinctFaultDisputeGame::new(game_impl_address, l1_provider.clone());
+    let anchor_state_registry_address = game_impl.anchorStateRegistry().call().await?;
+    let anchor_state_registry =
+        AnchorStateRegistry::new(anchor_state_registry_address, l1_provider.clone());
 
     let fetcher = OPSuccinctDataFetcher::new_with_rollup_config().await?;
     let host = initialize_host(Arc::new(fetcher.clone()));
@@ -69,11 +71,11 @@ async fn main() -> Result<()> {
 
     let proposer = Arc::new(
         OPSuccinctProposer::new(
-            ProposerConfig::from_env()?,
+            proposer_config,
             network_private_key,
-            prover_address,
             proposer_signer,
             factory,
+            anchor_state_registry,
             Arc::new(fetcher),
             host,
         )
