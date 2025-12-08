66f7502406b59fc0f4fa76e7f0d87774e7fd9fd2
Piers Powlesland <pierspowlesland@gmail.com>
2025-12-08 10:02:44 +0000

    Revert "Don't skip simulation in multi"
    This reverts commit 11852b4c2bc7eb030eb14751321b0ff7b9a5cf00.

--- scripts/prove/bin/multi.rs
+++ scripts/prove/bin/multi.rs
@@ -144,7 +144,7 @@ async fn main() -> Result<()> {
                 let proof = network_prover
                     .prove(&pk, &data.sp1_stdin)
                     .compressed()
-                    .skip_simulation(false)
+                    .skip_simulation(true)
                     .strategy(strategy)
                     .run_async()
                     .await
